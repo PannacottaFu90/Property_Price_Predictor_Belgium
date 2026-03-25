f7ad813e1a6f75746fb549907063422b5fbc6745
Edoardo Pizzileo <edoardo.pizzileo@gmail.com>
2026-03-25 15:45:03 +0100

    model training

---
+++ model_creator.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import numpy as np
+import joblib
+from sklearn.base import clone
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge, Lasso, ElasticNet
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import root_mean_squared_error
+from sklearn.metrics import mean_absolute_error, r2_score
+from sklearn.metrics import (
+    mean_squared_error,
+    mean_absolute_percentage_error,
+    median_absolute_error,
+    max_error,
+)
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, OrdinalEncoder
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
+from src.models import (
+    update_leaderboard,
+    linear_preprocessor,
+    prep_impute_ordinal,
+)
+from pathlib import Path
+import matplotlib.pyplot as plt
+from sklearn.model_selection import cross_val_score
+
+BASE_DIR = Path(__file__).resolve().parent
+
+htot_path = BASE_DIR / "data" / "3_data_for_training" / "df_h_tot.csv"
+clean_path = BASE_DIR / "data" / "3_data_for_training" / "df_clean.csv"
+atot_path = BASE_DIR / "data" / "3_data_for_training" / "df_a_tot.csv"
+
+df_clean = pd.read_csv(clean_path)
+df_h_tot = pd.read_csv(htot_path)
+df_a_tot = pd.read_csv(atot_path)
+
+pipeline1 = Pipeline(
+    [
+        ("prep", prep_impute_ordinal),
+        (
+            "reg",
+            XGBRegressor(
+                n_estimators=1000,
+                learning_rate=0.05,
+                max_depth=6,
+                random_state=42,
+            ),
+        ),
+    ]
+)
+
+# case total
+X = df_h_tot.drop("price", axis=1)
+y = df_h_tot["price"]
+
+df_temp = X.copy()
+df_temp["price_m2"] = y / X["livable_surface_m2"]
+final_zip_map_h = df_temp.groupby("zip_code")["price_m2"].mean()
+X["zip_code"] = X["zip_code"].map(final_zip_map_h)
+y_log = np.log1p(y)
+
+final_model_h = clone(pipeline1)
+final_model_h.fit(X, y_log)
+
+house_path = BASE_DIR / "model" / "model_house.pkl"
+h_zip_map_path = BASE_DIR / "model" / "zip_map_h.pkl"
+joblib.dump(final_model_h, house_path)
+joblib.dump(final_zip_map_h, h_zip_map_path)
+print("Modello house allenato e salvato con successo in model/model.pkl")
+
+# appartamenti total
+X = df_a_tot.drop("price", axis=1)
+y = df_a_tot["price"]
+
+df_temp = X.copy()
+df_temp["price_m2"] = y / X["livable_surface_m2"]
+final_zip_map_a = df_temp.groupby("zip_code")["price_m2"].mean()
+X["zip_code"] = X["zip_code"].map(final_zip_map_a)
+y_log = np.log1p(y)
+final_model_a = clone(pipeline1)
+final_model_a.fit(X, y_log)
+
+app_path = BASE_DIR / "model" / "model_apartament.pkl"
+a_zip_map_path = BASE_DIR / "model" / "zip_map_a.pkl"
+joblib.dump(final_zip_map_a, a_zip_map_path)
+joblib.dump(final_model_a, app_path)
+print("Modello appartamenti allenato e salvato con successo in model/model.pkl")
